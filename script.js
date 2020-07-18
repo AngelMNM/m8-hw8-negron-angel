0983e7083d2b3c1cd21ee0107a87553651daf0ce
Angel Negron <negron.a@ufl.edu>
2020-07-18 19:32:34 -0400

    Add alerts to js jquery file for practice

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     console.log("ready!");
+    alert("Click on blue box and it will move");
+
     $("#animBox").click(function() {
         $("#animBox").animate({ left: "50%" }, 800, "swing");
         $("#animBox").animate({ top: "50%" }, 1000, "swing");
         $("#animBox").animate({ left: "0%" }, 800, "swing");
-        $("#animBox").animate({ top: "0%" }, 1000, "swing");
+        $("#animBox").animate({ top: "0%" }, 1000, "swing", function() {
+            alert("Full animation complete");
+        });
+        console.log("done!");
     })
 });
